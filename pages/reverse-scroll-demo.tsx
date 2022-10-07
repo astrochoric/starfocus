5a0aec0a6b3d523319996d6af0b1e75dec6d16e2
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2022-10-07 14:03:15 +0100

    Full reverse scroll demo copied

--- pages/reverse-scroll-demo.tsx
+++ pages/reverse-scroll-demo.tsx
@@ -1,12 +1,29 @@
+import { useEffect } from 'react'
+
 export default function ReverseScrollDemo() {
 	const style = `
-		.box {
+		.box, .container {
+			position: relative;
 			height: 500px;
 			width: 500px;
-			background-color: red;
-			position: relative;
+			border: 1px solid black;
+			perspective-origin: top right;
+		}
 
+		.box {
+			width: 20px;
+			background-color: red;
+			position: absolute;
+			top: 0;
+
+			transform-origin: top right;
 			transform:
+				matrix3d(
+					1, 0, 0, 0,
+					0, 1, 0, 0,
+					0, 0, 1, 0,
+					0, 0, 0, -1
+				)
 				translateZ(-2px);
 			z-index: 1;
 		}
@@ -15,17 +32,24 @@ export default function ReverseScrollDemo() {
 			overflow: scroll;
 		}
 		.spacer {
-			display: block;
-			height: 800vh;
-			width: 100vw;
+			height: 1300px;
 		}
 	`
+	useEffect(() => {
+		const scroller = document.querySelector('.container')
+		const thumb = document.querySelector('.box')
+		const scrollerHeight = scroller.getBoundingClientRect().height
+		thumb.style.height =
+			(scrollerHeight * scrollerHeight) / scroller.scrollHeight + 'px'
+		thumb.style.right =
+			scroller.clientWidth - scroller.getBoundingClientRect().width + 'px'
+	}, [])
 	return (
 		<>
 			<style>{style}</style>
-			<div className="container h-screen w-screen border">
+			<div className="container">
 				<div className="box"></div>
-				<div className="spacer bg-planets bg-cover"></div>
+				<div className="spacer"></div>
 			</div>
 		</>
 	)
