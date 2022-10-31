05665c0dba808c0a4ea79875de43c528131b5558
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2022-10-31 17:18:40 +0000

    Add meta description

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -13,6 +13,13 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => {
 		<div>
 			<Head>
 				<title>{title}</title>
+				<meta
+					name="description"
+					content="For humanity to realise its full potential, you must realise yours.
+					Starfocus will help you discover your purpose and create a unique
+					constellation for it. Then we'll keep you on course as you embark
+					on your journey, one todo at a time."
+				/>
 				<meta charSet="utf-8" />
 				<meta
 					name="viewport"
