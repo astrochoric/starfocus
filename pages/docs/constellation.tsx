a0619c6e7b2acbb0f9efeff0f026be3f60f505d9
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2022-11-01 21:33:45 +0000

    Add docs page for constellations

---
+++ pages/docs/constellation.tsx
@@ -0,0 +1,17 @@
+import Layout from '../../components/common/Layout'
+import Feature from '../../components/landingPage/Features/Feature'
+
+export default function Constellation(props) {
+	return (
+		<Layout title="Constellation">
+			<Feature
+				id="constellation"
+				heading="constellation"
+			>
+				Your journey and purpose in life are completely unique to you and so is
+				your constellation. Your constellation is a sigil that you&apos;ll visit
+				frequently whilst using Starfocus to remind you of your mission.
+			</Feature>
+		</Layout>
+	)
+}
