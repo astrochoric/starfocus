43030cdcb4f070581458d1f95d7036382e6b0b13
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2023-02-15 21:07:19 +0000

    🚧 Add placeholder stats sidebar

--- components/app/PerformanceSidebar.tsx
+++ components/app/PerformanceSidebar.tsx
@@ -1,3 +1,41 @@
 export default function PerformanceSidebar() {
-	return <p>PerformanceSidebar</p>
+	return (
+		<div className="text-white">
+			<section className="mb-4">
+				<h1 className="text-xl font-black uppercase italic tracking-tight">
+					Velocity
+				</h1>
+				<div>
+					<span className="m-4 font-medium italic">Overall</span>
+					<code>8</code>
+				</div>
+				<div>
+					<span className="m-4 font-medium italic">Today</span>
+					<code>1</code>
+				</div>
+			</section>
+
+			<section className="mb-4">
+				<h1 className="text-xl font-black uppercase italic tracking-tight">
+					Roles
+				</h1>
+				<div>
+					<span className="m-4 font-medium italic">Software Creator</span>
+					<code>24%</code>
+				</div>
+				<div>
+					<span className="m-4 font-medium italic">Father</span>
+					<code>17%</code>
+				</div>
+				<div>
+					<span className="m-4 font-medium italic">Family Member</span>
+					<code>11%</code>
+				</div>
+				<div>
+					<span className="m-4 font-medium italic">Friend</span>
+					<code>9%</code>
+				</div>
+			</section>
+		</div>
+	)
 }
