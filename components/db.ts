1760b08332b2950725c29293d5a7524e573761a0
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2024-07-03 21:07:56 +0100

    ✨ Add Dexie Cloud integration

--- components/db.ts
+++ components/db.ts
@@ -1,4 +1,5 @@
 import Dexie, { Table } from 'dexie'
+import dexieCloud from 'dexie-cloud-addon'
 
 export interface Todo {
 	title: string
@@ -16,10 +17,16 @@ export class DexieStarfocus extends Dexie {
 	lists!: Table<List>
 
 	constructor() {
-		super('starfocus')
+		super('starfocus', {
+			addons: [dexieCloud],
+		})
 		this.version(1).stores({
-			todos: '++id, createdAt, completedAt, title',
+			todos: '@id, createdAt, completedAt, title',
 			lists: 'type', // Is this unique?
+		})
+		this.cloud.configure({
+			databaseUrl: 'https://zy0myinc2.dexie.cloud',
+			requireAuth: false,
 		})
 	}
 }
