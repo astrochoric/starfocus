396567f69bdeb6d4902d1f19d53ef0940772631b
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2024-07-04 22:56:29 +0100

    ✨ Add PWA manifest

---
+++ app/manifest.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+	return {
+		name: 'Starfocus',
+		short_name: 'Starfocus',
+		description: 'The todo list for our future',
+		start_url: '/home',
+		display: 'standalone',
+		background_color: '#fff',
+		theme_color: '#fff',
+		icons: [
+			{
+				src: '/logo.png',
+				sizes: 'any',
+				type: 'image/x-icon',
+			},
+		],
+	}
+}
